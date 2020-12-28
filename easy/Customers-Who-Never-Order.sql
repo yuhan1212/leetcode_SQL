7efd026265c9909312233e56bb91e4f55ad7cec5
yuhan1212 <yuhan.chi12@gmail.com>
2020-12-28 14:23:19 -0800

    rename using "as"

---
+++ easy/Customers-Who-Never-Order.sql
@@ -0,0 +1,42 @@
+'''
+
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+'''
+
+# Write your MySQL query statement below
+
+# Runtime: 362 ms, faster than 97.93%
+# Memory Usage: 0B, less than 100.00%
+
+Select Customers.Name as Customers
+From Customers
+Where Customers.Id not in ( Select CustomerID From Orders)
+
